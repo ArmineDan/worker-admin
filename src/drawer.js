5b480a208bdac45ef83c09c4207dd0d5e8479234
torosyan1 <torosyan252@gmail.com>
2019-08-04 18:22:18 +0400

    finishe_user

--- src/drawer.js
+++ src/drawer.js
@@ -6,35 +6,39 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers} from '@fortawesome/free-solid-svg-icons';
+import { faListUl} from '@fortawesome/free-solid-svg-icons';
 
 
 const drawerWidth = 240;
+
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
   },
+  
   drawer: {
     [theme.breakpoints.up('sm')]: {
       width: drawerWidth,
       flexShrink: 0,
+      
     },
   },
   appBar: {
     marginLeft: drawerWidth,
     [theme.breakpoints.up('sm')]: {
         
-      width: `calc(100% - ${drawerWidth}px)`,
+      width: `calc(100% - ${drawerWidth}px)`, 
     },
     
   },
@@ -42,19 +46,24 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
       display: 'none',
-    },
+      
+    }, 
   },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
     width: drawerWidth,
-    backgroundColor: "black",
+    backgroundColor: "Black",
     color: 'white',
   },
+
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
     
+    
   },
+  
+
 }));
 
 function ResponsiveDrawer(props) {
@@ -74,7 +83,7 @@ function ResponsiveDrawer(props) {
       <List>
         {["Users","Categries"].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>{index % 2 === 0 ? <FontAwesomeIcon icon={faUsers} style={{color: 'orange'}}  /> : <FontAwesomeIcon icon={faListUl} style={{color: 'orange'}} />}</ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -83,7 +92,7 @@ function ResponsiveDrawer(props) {
       <List>
         {[].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>{index % 2 === 0 ? <FontAwesomeIcon icon={faUsers} style={{color: 'orange'}}  /> : <FontAwesomeIcon icon={faListUl} style={{color: 'orange'}} />}</ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -106,12 +115,12 @@ function ResponsiveDrawer(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Admin (Armine Danielyan)
+            Admin 
           </Typography>
         </Toolbar>
       </AppBar>
       <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        
         <Hidden smUp implementation="css">
           <Drawer
             container={container}
@@ -123,7 +132,7 @@ function ResponsiveDrawer(props) {
               paper: classes.drawerPaper,
             }}
             ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
+              keepMounted: true, 
             }}
           >
             {drawer}
@@ -155,10 +164,7 @@ function ResponsiveDrawer(props) {
 }
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
+ 
   container: PropTypes.object,
 };
 
---
+++ src/userslist.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {getActiveUsers} from '../src/firebase/fireManager'
+
+
+
+class Users extends React.Component{
+    constructor(){
+        super();
+        this.state={
+          list: []    }
+    
+
+    }
+    
+      componentDidMount(){
+          
+    getActiveUsers().then(data=>{
+
+        this.setState({list: data})
+      
+    }) }
+   
+
+
+    render(){
+        let list_user='';
+        if(this.state.list ){
+            list_user =this.state.list.map((item,index)=>{ 
+                return(
+                  
+                    <tr key={index} id='customers'>
+                        <td>{++index}</td>
+                        <td> First Name : {item.firstName}</td>
+                        <td> Last Name : {item.lastName}</td>
+                        <td> Email: {item.email}</td>
+                        <td id='b'><button type='button'  >Archive</button> </td>
+                     </tr>
+                 )
+              })
+
+        }
+        
+    
+        
+        return(
+            <div> 
+           
+                 <table id="customers">
+                 <tr >
+                        <th>Numbering</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Permission</th>
+                    </tr>
+                     <tbody>
+                     {list_user}
+
+                     </tbody>
+                </table> 
+        
+                    
+                
+                 </div>
+        )
+    }
+
+}
+
+
+export default Users;
